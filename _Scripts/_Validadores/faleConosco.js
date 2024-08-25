7288849c9846d4a0af1d05a7b2c817863af3f096
Ana Quintão <ana.quintao@localiza.com>
2024-08-25 11:06:19 -0300

    Atualização validadores Java da aba Fale Conosco
    Atualização validadores Java da aba Fale Conosco

--- _Scripts/_Validadores/faleConosco.js
+++ _Scripts/_Validadores/faleConosco.js
@@ -1,8 +1,7 @@
 // configurações da aba Fale conosco //
 
 // capturar o elemento Assunto da aba Fale conosco
-let titulo = document.getElementById("textoTamanhoDinamico")
-
+let titulo = document.getElementById("adicionarInfoComentario")
 
 titulo.addEventListener("input", (assuntoComentario) => {
     console.log(assuntoComentario.target.value)
@@ -13,9 +12,38 @@ titulo.addEventListener("input", (assuntoComentario) => {
             titulo.style.background = "green"
         } else {
             titulo.style.background = "red"
-            window.alert("Assunto é obrigatório");
         }
     }
     else
         titulo.style.background = "#156669"
+})
+
+// capturar o elemento Mensagem da aba Fale conosco
+let mensagem = document.getElementById("textoTamanhoDinamico")
+
+mensagem.addEventListener("input", (mensagemComentario) => {
+    console.log(mensagemComentario.target.value)
+    let valorImpt2 = mensagemComentario.target.value
+    if(valorImpt2.length != 0)
+    {
+        if(valorImpt2.length > 3  ){
+            mensagem.style.background = "green"
+        } else {
+            mensagem.style.background = "red"
+        }
+    }
+    else
+        mensagem.style.background = "#156669"
+})
+
+// captura botao enviar
+const bntEnviar = document.querySelector('input[type="submit"].btnPadrao');
+
+// Alerta de falha de preenchimento ou envio correto
+bntEnviar.addEventListener("click", () => {
+    if (titulo.value === "" || mensagem.value === "") {
+        alert('Necessário preenchimento dos campos para envio');
+    } else {
+        alert('Formulário enviado com sucesso')
+    }
 })
